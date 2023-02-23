37d845f7b1616210461848244ccc4544f4a4072d
Jar'Zeno <jarzenojarrett@gmail.com>
2023-02-23 07:59:38 -0800

    Init npm

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+require('dotenv').config();
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,7 +7,14 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.write(`
+        <h1>Hello Zeno!</h1>
+        <p>${process.env.USER_ID}</p>
+        <p>${process.env.USER_KEY}</p>
+        <p>${process.env.NODE_ENV}</p>
+        <p>I see you</p>
+    `);
+    res.end();
 });
 
 server.listen(port, hostname, () => {
